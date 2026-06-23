7eb3defc688444b0d58ad12640d226031db5d6d2
ounben <oun@outlook.com>
2026-06-23 22:35:58 +0200

    cleanup ConnectivityChecker.kt

--- app/src/main/kotlin/com/ounben/amaradio/service/ConnectivityChecker.kt
+++ app/src/main/kotlin/com/ounben/amaradio/service/ConnectivityChecker.kt
@@ -5,7 +5,6 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
-import androidx.core.net.ConnectivityManagerCompat
 
 class ConnectivityChecker {
     enum class ConnectionType {
