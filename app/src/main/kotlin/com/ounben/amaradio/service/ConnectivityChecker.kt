1540102fc832a5c60888e9c9c1458e74d2fb1a1f
ounben <oun@outlook.com>
2026-06-23 21:04:46 +0200

    cleanup ConnectivityChecker.kt

--- app/src/main/kotlin/com/ounben/amaradio/service/ConnectivityChecker.kt
+++ app/src/main/kotlin/com/ounben/amaradio/service/ConnectivityChecker.kt
@@ -1,14 +1,10 @@
 package com.ounben.amaradio.service
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
-import android.os.Build
 import androidx.core.net.ConnectivityManagerCompat
 
 class ConnectivityChecker {
@@ -23,18 +19,17 @@ class ConnectivityChecker {
 
     private var connectivityManager: ConnectivityManager? = null
     private var networkCallback: ConnectivityManager.NetworkCallback? = null
-    private var networkBroadcastReceiver: BroadcastReceiver? = null
     private var connectivityCallback: ConnectivityCallback? = null
     private var lastConnectionType: ConnectionType? = null
 
     fun startListening(context: Context, connectivityCallback: ConnectivityCallback) {
         this.connectivityCallback = connectivityCallback
-        if (networkCallback != null || networkBroadcastReceiver != null) {
+        if (networkCallback != null) {
             return
         }
         lastConnectionType = getCurrentConnectionType(context)
         connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && connectivityManager != null) {
+        if (connectivityManager != null) {
             networkCallback = object : ConnectivityManager.NetworkCallback() {
                 override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
                     val connected = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
@@ -43,27 +38,15 @@ class ConnectivityChecker {
                 }
             }
             connectivityManager!!.registerNetworkCallback(NetworkRequest.Builder().build(), networkCallback!!)
-        } else {
-            networkBroadcastReceiver = object : BroadcastReceiver() {
-                override fun onReceive(context: Context, intent: Intent) {
-                    val connected = !intent.hasExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY)
-                    onConnectivityChanged(connected, if (ConnectivityManagerCompat.isActiveNetworkMetered(connectivityManager!!)) ConnectionType.METERED else ConnectionType.NOT_METERED)
-                }
-            }
-            @Suppress("DEPRECATION")
-            context.registerReceiver(networkBroadcastReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
         }
     }
 
     fun stopListening(context: Context) {
         connectivityCallback = null
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && networkCallback != null) {
+        if (networkCallback != null) {
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             connectivityManager.unregisterNetworkCallback(networkCallback!!)
             networkCallback = null
-        } else if (networkBroadcastReceiver != null) {
-            context.unregisterReceiver(networkBroadcastReceiver)
-            networkBroadcastReceiver = null
         }
     }
 
@@ -79,7 +62,7 @@ class ConnectivityChecker {
         @JvmStatic
         fun getCurrentConnectionType(context: Context): ConnectionType {
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            return if (ConnectivityManagerCompat.isActiveNetworkMetered(connectivityManager)) ConnectionType.METERED else ConnectionType.NOT_METERED
+            return if (connectivityManager.isActiveNetworkMetered) ConnectionType.METERED else ConnectionType.NOT_METERED
         }
     }
 }
